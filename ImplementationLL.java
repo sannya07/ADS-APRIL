9c7d9f5d5ce8ca894cc262e3b82545f9404aab69
sannya07 <sanyakansal07@gmail.com>
2025-04-15 22:44:31 +0530

    new update

--- ImplementationLL.java
+++ ImplementationLL.java
@@ -47,16 +47,95 @@ public class ImplementationLL {
 
         // insert at a particular index
         void insert(int index,int val){
-            
+            if(index==0){
+                insertAtHead(val);
+                return;
+            }
+            if(index==size){
+                insertAtEnd(val);
+                return;
+            }
+            if(index>size){
+                System.out.println("invalid index");
+            }
+            Node temp=new Node(val);
+            Node x=head;
+            for(int i=0;i<index-1;i++){
+                x=x.next;
+            }
+            temp.next=x.next;
+            x.next=temp;
+            size++;
+        }
+
+        int get(int index) throws Exception{
+            if(index==size-1) return tail.val;
+            if(index>=size || index<0){
+                throw new Exception("Invalid index");
+            }
+            Node temp=head;
+            for(int i=1;i<=index;i++){
+                temp=temp.next;
+            }
+            return temp.val;
+        }
+
+        void deleteAtHead() throws Error{
+            if(head==null) throw new Error("List is empty!!");
+            head=head.next;
+            size--;
+        }
+
+        void deleteAtTail() throws Error{
+            if(head==null) throw new Error("List is empty!!");
+            if(head==tail){
+                head=tail=null;
+            }else{
+                Node current=head;
+                while(current.next.next!=null){
+                    current=current.next;
+                }
+                current.next=null;
+            }
+            size--;
+        }
+
+        // delete at a particular index
+        void delete(int index)throws Error{
+            if(head==null) throw new Error("List is empty!!");
+            if(index>=size || index<0) throw new Error("invalid index");
+            Node temp=head;
+            for(int i=1;i<=index-1;i++){
+                temp=temp.next;
+            }
+            temp.next=temp.next.next;
+            size--;
         }
     }
     public static void main(String[] args) {
         SLL list=new SLL();
+        // list.deleteAtHead();
         list.insertAtEnd(10);
         list.insertAtEnd(20);
         list.insertAtEnd(30);
         list.insertAtHead(100);
         list.insertAtHead(55);
+        list.insert(2, 99);
+        // to get an element in a linked list time complexity is O(N)
+        try {
+            list.get(2);
+            list.display();
+            System.out.println();
+            System.out.println(list.size);
+            System.out.println(list.get(2));
+            list.deleteAtHead();
+            list.display();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        list.deleteAtTail();
+        System.out.println();
+        list.delete(3);
         list.display();
         System.out.println();
         System.out.println(list.size);
